cd9fa504bf1b190643bcb6bcc15488baa6e31e6c
Sid Bentifraouine <sidbentifraouine@gmail.com>
2019-01-06 16:45:30 +0100

    Some refacto

--- run-label.sh
+++
@@ -1,6 +0,0 @@
-python3 label_image.py \
---graph=/tmp/output_graph.pb --labels=/tmp/output_labels.txt \
---input_layer=Placeholder \
---output_layer=final_result \
-$1
-# --image=$HOME/building-photos/istv-3/istv-3-back.jpg
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const exec = require('child_process').exec
 
+console.log('🚀  Executing label_image.py')
 exec(
-  './run-label.sh --image=$HOME/building-photos/istv-3/istv-3-back.jpg',
+  `python3 label_image.py \
+    --graph=/tmp/output_graph.pb --labels=/tmp/output_labels.txt \
+    --input_layer=Placeholder \
+    --output_layer=final_result \
+    --image=$HOME/building-photos/istv-2/istv-2-back.jpg`,
   (error, stdout, stderr) => {
-    console.log(`${stdout}`)
+    console.log('🎬  Finished label_image.py execution')
+    console.log(`✅  Result:\n${stdout}`)
     const result = stdout.slice(0, 6)
-    console.log(result)
-    // console.log(`${stderr}`)
+    console.log(`✅  The building is: ${result}\n`)
+    // Errors handling
+    console.error(`❌  ${stderr}`)
     if (error !== null) {
-      console.log(`exec error: ${error}`)
+      console.error(`❌  Exec error: ${error}`)
     }
   },
 )
